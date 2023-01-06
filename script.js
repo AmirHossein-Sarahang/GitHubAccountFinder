3df31b9f2cfdbcdbfccb6a79f321695f4db5a64d
AmirHossein <amirsrhng@gmail.com>
2023-01-06 21:19:10 +0330

    UPDATE JS !! cm

--- script.js
+++ script.js
@@ -1,51 +1,61 @@
+/*
+Our main function is called from Html
+ */
 function Main(){
     var username = document.getElementById("t1").value;
     var url = "https://api.github.com/users/"
     url += username
 
-    if (getCookie("User") === username){
+    if (getCookie("User") === username){ //Read from cookies and set in html with getCookie and set Info and then call error handler for print from cookie
         setInfo(getCookie("User"), getCookie("Blog"), getCookie("Location"), getCookie("Bio"), getCookie("Img") )
         ErrorsHandler("Cookie");
 
     }
-    else{
+    else{//set information without cookies in handler function
         Handler(url)
     }
 
 
 }
+//fetch data and convert to json
 function Handler(url){
     document.getElementById("error").innerHTML = "<br>"
     fetch(url).then(function(response) {
         return response.json();
-    }).then(function(data) {
+    }).then(function(data) {//if user field in data is Not found set info to default and print not found by error handler
         if (data.message === 'Not Found'){
             SetDefault();
             ErrorsHandler("NotFound");
         }
         else {
+            //if user exists then :
             let bio = data.bio;
             if (bio == null){
                 bio = "";
             }
             else{
                 bio = bio.replace("\n", "<br/>");
+                //set bio in bio and replace \n to br for new line
             }
+            //then set info and cookies and then call  WhatsFavoriteLanguage for check repos and find FavoriteLanguage
             setInfo(data.login, data.blog, data.location, bio, data.avatar_url)
             setCookies(data.login, data.blog, data.location, data.avatar_url, bio)
             WhatsFavoriteLanguage(data.repos_url)
-            ErrorsHandler("New")
+            ErrorsHandler("New") // for print new request
 
         }
 
 
-        //console.log(data);
     }).catch(function(err) {
+        //network error
        ErrorsHandler("Network");
         console.log('Fetch Error :-S', err);
     });
 }
-
+/*
+to find FavoriteLanguage we have to data s length
+and then we check length value in if because we need just 5 no more!
+ */
 function WhatsFavoriteLanguage(URL){
     fetch(URL).then(function(response) {
         return response.json();
@@ -57,15 +67,17 @@ function WhatsFavoriteLanguage(URL){
         else {
             length = 5;
         }
-
+        // then we make 2 arrays : languages = copy of first 5 language and cn for counters
         const languages = ["", "", "", "", ""]
         for (let i = 0; i < length; i++) {
             if (data[i].language) {
                 languages[i] = data[i].language
+                //copy first 5 language
             }
         }
 
         const cn = [0, 0, 0, 0, 0]
+        // Checking the number of each and adding the appropriate counter :
         for (let i = 0; i < length; i++) {
             if (data[i].language) {
                 switch (data[i].language) {
@@ -88,14 +100,15 @@ function WhatsFavoriteLanguage(URL){
                 }
             }
         }
+        //then we find max value in counters and pass it to languages as index :
         let index = cn.indexOf(Math.max(...cn));
         var FL = "Favorite language : ";
         FL += languages[index];
         setLanguage(languages[index])
-        document.getElementById("language").innerHTML = FL;
+        document.getElementById("language").innerHTML = FL; //set inner html
 
         for (let i = 0; i < 5; i++) {
-            languages[i] = "";
+            languages[i] = ""; //reset languages array
         }
 
 
@@ -103,7 +116,7 @@ function WhatsFavoriteLanguage(URL){
     });
 
 }
-
+// just sets information to Default values
 function SetDefault(){
     document.getElementById("user").innerHTML= "Username"
     document.getElementById("img").src= "https://cdn.iconscout.com/icon/free/png-256/avatar-370-456322.png"
@@ -113,7 +126,7 @@ function SetDefault(){
     document.getElementById("language").innerHTML="Favorite language";
     document.getElementById("error").innerHTML="<br>"
 }
-
+// find special cookie bu name and getting help from the split function To separate fields
 function getCookie(cname) {
     let name = cname + "=";
     let decodedCookie = decodeURIComponent(document.cookie);
@@ -129,7 +142,7 @@ function getCookie(cname) {
     }
     return "";
 }
-
+// set cookies with expires time (60000ms = 60s) and convert it to string and then set cookies
 function setCookies(name, blog, location, img, bio){
     deleteAllCookies()
     var date = new Date();
@@ -141,14 +154,14 @@ function setCookies(name, blog, location, img, bio){
     document.cookie = "Img" + "=" + img + ";" + expires + ";path=/";
     document.cookie = "Bio" + "=" + bio + ";" + expires + ";path=/";
 }
-
+// set Language cookie like others with 60s expire time
 function setLanguage(lang){
     var date = new Date();
     date.setTime(date.getTime() + (60 * 1000));
     let expires = "expires="+ date.toUTCString();
     document.cookie = "Language" + "=" + lang + ";" + expires + ";path=/";
 }
-
+//delete All Cookie for reset everything to Default about cookies
 function deleteAllCookies() {
     const cookies = document.cookie.split(";");
 
@@ -159,7 +172,7 @@ function deleteAllCookies() {
         document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
     }
 }
-
+// set all fields in html to their new quantity
 function setInfo(name, blog, loc, bio, img){
     document.getElementById("user").innerHTML= name
     document.getElementById("img").src= img
@@ -168,7 +181,7 @@ function setInfo(name, blog, loc, bio, img){
     document.getElementById("bio").innerHTML= bio
     document.getElementById("error").innerHTML="<br>"
 }
-
+// error handler use switch case to set inner html to special error
 function ErrorsHandler(err){
     switch (err) {
         case "New":
